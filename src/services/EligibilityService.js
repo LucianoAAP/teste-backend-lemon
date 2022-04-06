b04f637660d5db910218b65df6360a90a09c56a3
Luciano Almeida <78364395+LucianoAAP@users.noreply.github.com>
2022-04-06 02:19:23 -0300

    Validations (#5)
    * FEAT: adds request validations

--- src/services/EligibilityService.js
+++ src/services/EligibilityService.js
@@ -1,6 +1,11 @@
 const EligibilityModel = require('../models/EligibilityModel');
+const { badRequest } = require('../error/apiError');
+const validateClient = require('../validations/ValidateClient');
 
 const createEligibilityReport = (client) => {
+  const error = validateClient(client);
+  if (error) return badRequest(error);
+
   const eligibilityReport = EligibilityModel.createEligibilityReport(client);
   return eligibilityReport;
 };
