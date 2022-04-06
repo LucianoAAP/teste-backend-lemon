05ffc2ac841a0eb12d442caac32b1af10b8c2f92
Luciano AAP <lucianoalmeidaap@gmail.com>
2022-04-06 02:44:32 -0300

    TEST: adds tests for missing request keys

--- src/tests/integration/createEligibilityReport.test.js
+++ src/tests/integration/createEligibilityReport.test.js
@@ -1,6 +1,7 @@
+const sinon = require('sinon');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const { OK } = require('http-status-codes');
+const { OK, BAD_REQUEST } = require('http-status-codes');
 const server = require('../../api/app');
 const eligibleClient = require('../mocks/eligible');
 const notEligibleClient = require('../mocks/notEligible');
@@ -14,6 +15,36 @@ const { expect } = chai;
 describe('Testa rota de criação de relatórios de eligibilidade', () => {
   const expectedNotEligible = JSON.parse(JSON.stringify(notEligibleClientReport));
   const expectedEligible = JSON.parse(JSON.stringify(eligibleClientReport));
+
+  describe('Testa com propriedades faltando na requisição', () => {
+    const keys = Object.keys(eligibleClient);
+
+    keys.forEach((key) => {
+      describe(`Testa sem a propriedade ${key} na requisição`, () => {
+        const client = {};
+        keys.filter((property) => property !== key).forEach((property) => {
+          client[property] = eligibleClient[property];
+        });
+
+        const expectedMessage = `\"${key}\" is required`;
+
+        let response = {};
+
+        before(async () => {
+          if (!console.log.restore) sinon.stub(console, "log");
+          response = await chai.request(server).post('/eligibility').send(client);
+        });
+
+        it('A resposta possui o status correto', () => {
+          expect(response).to.have.status(BAD_REQUEST);
+        });
+
+        it('Retorna a mensagem de erro correta', () => {
+          expect(response.body).to.be.deep.equal(expectedMessage);
+        });
+      });
+    });
+  });
 
   describe('Testa com uma pessoa não elegível', () => {
     let response = {};
